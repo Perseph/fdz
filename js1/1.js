4265465b0ba60b7dedc950cc93215be9a190223b
Perseph <lexashulichenko@gmail.com>
2018-03-05 19:49:05 +0300

    Add files via upload

---
+++ js1/1.js
@@ -0,0 +1,25 @@
+
+alert( "Привет" );
+
+var firstName = 'Alex';
+var lastName = firstName;
+
+document.write(lastName);
+
+
+
+
+// var a = parseInt(prompt('Число А', ''));
+// var b = parseInt(prompt('Число Б', ''));
+// var c = (a + b);
+//
+// alert('Сумма = ' + c);
+
+
+/*
+var firstName = 'Alex';
+var lastName = 'Shulichenko";
+var age = 21;
+var education = 'Bachelor';
+var hobby = 'moto-sport';
+ */
